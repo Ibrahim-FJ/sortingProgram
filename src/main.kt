a16c73b8b28a503f71ecde173667d8c64614122d
Ibrahim Alfaifi <Ibrahim.farhan.ruh.k@tuwaiq.edu.sa>
2021-10-13 16:29:39 +0300

    Changing the code using the functions

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,43 @@
 fun main (){
     println("Please Enter three number to sort them from the smallest to the biggest")
-    var number1 = readLine()!!.toFloat()
-    var number2 = readLine()!!.toFloat()
-    var number3 = readLine()!!.toFloat()
+    val number1 = readLine()!!.toFloat()
+    val number2 = readLine()!!.toFloat()
+    val number3 = readLine()!!.toFloat()
 
+
+//    if (smallest > number2){
+//        smallest = number2
+//    }
+//    if (smallest >= number3){
+//        smallest = number3
+//    }
+
+
+//    if(biggest < number2){
+//        biggest = number2
+//    }
+//    if(biggest <= number3){
+//        biggest = number3
+//    }
+
+//    if (number1 < biggest && number1 > smallest){
+//        mid = number1
+//    }else if (number2 < biggest && number2 > smallest){
+//        mid = number2
+//    }else{
+//        mid = number3
+//    }
+
+        val smallest = getSmallest(number1, number2, number3)
+        val biggest = getBiggest(number1, number2, number3)
+        val mid = getMid(number1, number2, number3, smallest, biggest)
+
+        println("$smallest\n $mid\n $biggest")
+
+}
+
+fun getSmallest(number1 : Float, number2 : Float, number3: Float) : Float{
     var smallest : Float = number1
-    var biggest : Float = number1
-    var mid : Float ?= null
-
-
     if (smallest > number2){
         smallest = number2
     }
@@ -16,6 +45,11 @@ fun main (){
         smallest = number3
     }
 
+   return smallest
+}
+
+fun getBiggest(number1 : Float, number2 : Float, number3: Float) : Float{
+    var biggest : Float = number1
 
     if(biggest < number2){
         biggest = number2
@@ -23,7 +57,12 @@ fun main (){
     if(biggest <= number3){
         biggest = number3
     }
+    return biggest
 
+}
+
+fun getMid(number1 : Float, number2 : Float, number3: Float, smallest : Float, biggest : Float) : Float{
+    var mid : Float ?= null
     if (number1 < biggest && number1 > smallest){
         mid = number1
     }else if (number2 < biggest && number2 > smallest){
@@ -31,7 +70,5 @@ fun main (){
     }else{
         mid = number3
     }
-
-println("$smallest\n $mid\n $biggest")
-
+    return mid
 }
