1e5cac692f2792132efb0b74f94f1c335e8a7a2f
Ibrahim Alfaifi <Ibrahim.farhan.ruh.k@tuwaiq.edu.sa>
2021-10-14 11:14:17 +0300

    Add boolean condition to specify which function to use

--- src/Helpers.kt
+++ src/Helpers.kt
@@ -1,4 +1,28 @@
 
+fun sort(number1: Float, number2: Float, number3: Float){
+
+    println("Enter true or false to use which function")
+    val easyMid = readLine()!!.toBoolean()
+    val smallest = getSmallest(number1, number2, number3)
+    val biggest = getBiggest(number1, number2, number3)
+
+    if (easyMid) {
+
+        val mid = getMid1(number1, number2, number3)
+        println("Mid from 1st mid\n$smallest\n$mid\n$biggest")
+    }else{
+
+        val mid = getMid2(number1, number2, number3)
+        println("Mid from 2st mid\n$smallest\n$mid\n$biggest")
+    }
+
+
+}
+
+
+
+
+
 fun getSmallest(number1 : Float, number2 : Float, number3: Float) : Float{
     var smallest : Float = number1
     if (smallest > number2){
@@ -24,7 +48,24 @@ fun getBiggest(number1 : Float, number2 : Float, number3: Float) : Float{
 
 }
 
-fun getMid(number1 : Float, number2 : Float, number3: Float, smallest : Float, biggest : Float) : Float{
+fun getMid1(number1 : Float, number2 : Float, number3: Float) : Float{
+    var mid : Float ?= null
+    val smallest = getSmallest(number1, number2, number3)
+    val biggest = getBiggest(number1, number2,number3 )
+
+    if (number1 < biggest && number1 > smallest){
+        mid = number1
+    }else if (number2 < biggest && number2 > smallest){
+        mid = number2
+    }else{
+        mid = number3
+    }
+    return mid
+}
+fun getMid2 (number1 : Float, number2 : Float, number3: Float) : Float{
+ val smallest = getSmallest(number1, number2, number3)
+    val biggest = getBiggest(number1, number2,number3 )
+
     var mid : Float ?= null
     if (number1 < biggest && number1 > smallest){
         mid = number1
--- src/main.kt
+++ src/main.kt
@@ -3,12 +3,8 @@ fun main (){
     val number1 = readLine()!!.toFloat()
     val number2 = readLine()!!.toFloat()
     val number3 = readLine()!!.toFloat()
+    sort(number1, number2, number3)
 
 
-        val smallest = getSmallest(number1, number2, number3)
-        val biggest = getBiggest(number1, number2, number3)
-        val mid = getMid(number1, number2, number3, smallest, biggest)
-
-        println("$smallest\n $mid\n $biggest")
 
 }
